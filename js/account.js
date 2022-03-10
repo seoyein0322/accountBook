7025811d653ed79b8d265f4611ff92f5b4cf2b50
seoyein0322 <69830160+seoyein0322@users.noreply.github.com>
2022-03-10 20:39:42 +0900

    js/account.js chooseTypeAndOption, createHistoryList함수 추가

--- js/account.js
+++ js/account.js
@@ -1,10 +1,16 @@
+const historyList = document.querySelector(".history-list");
+const amount = document.querySelector(".amount");
+const memo = document.querySelector(".memo");
 const totalAmount = document.querySelector(".total-amount");
 const totalIncome = document.querySelector(".total-income");
 const totalExpend = document.querySelector(".total-expend");
+const typeSelect = document.querySelector("#type-select");
 const typeIncome = document.querySelector("#type-income");
 const typeExpand = document.querySelector("#type-expand");
 const incomeOption = ["월급", "용돈", "지원금", "저축", "기타"];
 const expendOption = ["쇼핑", "여가생활", "교통/차량", "식비", "기타"];
+let amountVal = amount.value;
+let memoVal = memo.value;
 let icome = 0;
 let expend = 0;
 let total = icome + expend;
@@ -19,11 +25,30 @@ function createTotalContents() {
     totalExpend.insertAdjacentHTML("beforeend", expend);
 }
 
-function chooseTypeAndOption() {}
+function countAccount() {}
+
+function chooseTypeAndOption() {
+    const options = ``;
+    if (typeIncome.checked) {
+        for (let i = 0; i <= incomeOption.length; i++) {
+            options += `<option value="${incomeOption[i]}">${incomeOption[i]}</option>`;
+        }
+    } else if (typeExpand.checked) {
+        for (let i = 0; i <= expendOption.length; i++) {
+            options += `<option value="${expendOption[i]}">${expendOption[i]}</option>`;
+        }
+    }
+    typeSelect.insertAdjacentHTML("afterend", options);
+}
 
 function clickHistoryCategory() {}
 
-function createHistoryList() {}
+function createHistoryList() {
+    const list = ``;
+    list += `<li><div class="list-amount">${amountVal}</div><div class="list-memo">${memoVal}</div><button class="btn delete"></button></li>`;
+
+    historyList.insertAdjacentHTML("afterend", list);
+}
 
 function AmountCommas(num) {
     return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
